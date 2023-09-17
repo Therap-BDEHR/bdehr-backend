28b47029a0e9ff8e363a8f185ebae8c430d25cdc
Saadmrp1038 <saadmrp222@gmail.com>
2023-09-17 20:27:52 +0600

    Typo Fixed

--- src/main/java/com/bdehr/backend/Service/ReportService.java
+++ src/main/java/com/bdehr/backend/Service/ReportService.java
@@ -1,5 +1,6 @@
 package com.bdehr.backend.Service;
 
+import com.bdehr.backend.Interface.ReportRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,5 +9,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ReportService {
     @Autowired
-    private ReportService reportService;
+    private ReportRepository reportRepo;
 }
