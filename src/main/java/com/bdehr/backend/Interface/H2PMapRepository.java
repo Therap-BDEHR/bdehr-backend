4dd7604a4cada081bb8166325ae077afacd266f9
Saadmrp1038 <saadmrp222@gmail.com>
2023-09-17 20:16:57 +0600

    Mapping Added

--- src/main/java/com/bdehr/backend/Interface/H2PMapRepository.java
+++ src/main/java/com/bdehr/backend/Interface/H2PMapRepository.java
@@ -1,6 +1,7 @@
 package com.bdehr.backend.Interface;
 
+import com.bdehr.backend.Entity.H2PMap;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface H2PMapRepository extends JpaRepository<H2PMapRepository, Integer> {
+public interface H2PMapRepository extends JpaRepository<H2PMap, Integer> {
 }
